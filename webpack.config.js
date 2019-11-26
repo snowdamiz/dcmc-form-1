395b5004c9da907acbc3b46d45e263978c5935f3
120356aa <yurlovandrew@gmail.com>
2019-11-26 06:28:29 -0800

    manual deploy vie webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const UglyfiJsPlugin = require("uglifyjs-webpack-plugin");
+const glob = require("glob");
+
+module.exports = {
+    entry: ['@babel/polyfill','./src/index.js'],
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [new UglyfiJsPlugin()]
+    }
+}
