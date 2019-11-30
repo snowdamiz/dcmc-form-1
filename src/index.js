45506968dcd0558d6e9c5e02b7e779e9772a646b
120356aa <yurlovandrew@gmail.com>
2019-11-30 15:37:06 -0800

    timeout

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// setTimeout(() => {
-//     ReactDOM.render(<App />, document.getElementById('root'));
-// }, 30000 );
-
-ReactDOM.render(<App />, document.getElementById('root'));
+setTimeout(() => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}, 30000 );
